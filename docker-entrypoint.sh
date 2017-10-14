5dbb86e00390bffcca561d1a7f9bbdb89b0ef29b
Cheng-Lung Sung <clsung@gmail.com>
2017-10-14 16:30:35 +0800

    fix ipc problem, v1.1

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,7 +9,9 @@ exec su-exec root tcp-proxy -l="0.0.0.0:${LOCALPORT}" -r="${REMOTEADDR}" &
 
 # setup vpn
 if [ $# -gt 0 ] && [ "$1" = 'openfortivpn' ]; then
-    exec su-exec root "$@"
+#    exec su-exec root "$@"
+#   above will cause ipc not working (i.e. sem-post not fire or sem-wait not received)
+    exec "$@"
 fi
 
 exec "$@"
